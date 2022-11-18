b12d484b9f1f98d7bbefdf0e695e960b0e87786b
Soufiane El Alami <soufianealami06@gmail.com>
2022-11-18 23:12:14 +0100

    add implementation for OperationForStudent

---
+++ src/main/java/services/ImplOperationForStudent.java
@@ -0,0 +1,20 @@
+package services;
+
+import model.Student;
+
+public class ImplOperationForStudent implements OperationForStudent{
+    @Override
+    public void addStudent(Student student) {
+        
+    }
+
+    @Override
+    public void removeStudentById(Long id) {
+
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+}
